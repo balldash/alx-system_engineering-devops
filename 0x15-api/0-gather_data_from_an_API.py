41774dd735cd1f0350fcfd8d0f9c55666f00afa5
Bislon Zulu <bislonzulu1@outlook.com>
2024-07-30 19:47:53 +0200

    task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+import requests
+import sys
+
+def employee_todo_progress(employee_id):
+    """
+    Feth and display the TODO list progress for a given employee ID.
+    """
+    employee_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    employee_response = requests.get(employee_url)
+    employee_data = employee_response.json()
+
+    todos_url = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
+    todos_response = requests.get(todos_url)
+    todos_data = todos_response.json()
+
+    employee_name = employee_data['name']
+
+    total_tasks = len(todos_data)
+    done_tasks = [task for task in todos_data if task['completed']]
+    number_of_done_tasks = len(done_tasks)
+
+    print(f"Employee {employee_name} is done with tasks({number_of_done_tasks}/{total_tasks}):")
+    for task in done_tasks:
+        print(f"\t {task['title']}")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <employee_id>")
+        sys.exit(1)
+
+    try:
+        employee_id = int(sys.argv[1])
+        employee_todo_progress(employee_id)
+    except ValueError:
+        print("Employee ID must be an integer.")
+        sys.exit(1)

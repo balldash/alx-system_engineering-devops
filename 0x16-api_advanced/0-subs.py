decef51ce11ac22665832f53855d40bb65e61e6c
Bislon Zulu <bislonzulu1@outlook.com>
2024-09-05 22:15:58 +0200

    tsk 0 fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,16 +3,17 @@
 Task 0 module
 """
 
+
 def number_of_subscribers(subreddit):
     """
     Queries the Reddit API and returns the number of subscribers
     """
     import requests
 
-    info = requests.get("https://www.reddit.com/r/{}/about.json"
-            .format(subreddit),
-            headers={"User-Agent": "My-User-Agent"},
-            allow_redirects=False)
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    a = 'User-Agent'
+    b = 'My-User-Agent'
+    info = requests.get(url, headers={a: b}, allow_redirects=False)
     if info.status_code >= 300:
         return 0
     return info.json().get("data").get("subscribers")

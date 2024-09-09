cb7d9e05134f61f640d0e767c576e0b5d2200ba4
Bislon Zulu <bislonzulu1@outlook.com>
2024-09-09 15:18:55 +0300

    task

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,6 @@
 # Solve the failed requests to the server
 
+# Increase the upper limit
 exec { 'fix--for-nginx':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'

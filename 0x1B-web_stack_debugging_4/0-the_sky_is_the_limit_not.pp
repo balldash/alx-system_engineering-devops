dc8004410b7c5ef2802ca3f98e2775ab1f5d8376
Bislon Zulu <bislonzulu1@outlook.com>
2024-09-09 15:26:49 +0300

    tasks 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,13 +1,17 @@
 # Solve the failed requests to the server
 
-# Increase the upper limit
+#increase the ULIMIT of the default file
 exec { 'fix--for-nginx':
-  command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => '/usr/local/bin/:/bin/'
-} ->
+    # Modify the ULIMIT value
+    command => '/bin/sed -i "s/15/4096/" /etc/default/nginx',
+    # specify the path for the sed command
+    path    => '/usr/local/bin/:/bin/',
+}
 
 # Restart Nginx
 exec { 'nginx-restart':
-  command => 'nginx restart',
-  path    => '/etc/init.d/'
+  # Restart Nginx service
+  command => '/etc/init.d/nginx restart',
+  # Specify the path for init.d script
+  path    => '/etc/init.d/',
 }

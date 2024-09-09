bab61409c25dadcdf071a374695158c56c87a8fb
Bislon Zulu <bislonzulu1@outlook.com>
2024-09-09 13:54:24 +0300

    task 0.6

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,7 +12,7 @@ def number_of_subscribers(subreddit):
     """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "My-User-Agent"}
-    response = requests.get(url, header=headers, allow_redirects=False)
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code >= 300:
         return 0
     return response.json().get("data", {}).get("subscribers", 0)
